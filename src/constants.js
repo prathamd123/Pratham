bddc0c2c47b36c24f7e26126e026c81c059cb082
pratham devatwal <prathamdevatwal0@gmail.com>
2024-06-30 23:18:33 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Front-End Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Pratham';
+
+export const experiences = [
+    {
+        'company': 'Unified Mentor',
+        'role': 'Full Stack Developer Intern',
+        'duration': 'June 2024 - Present',
+        'logo': 'https://www.unifiedmentor.com/img/logo-blue.png',
+        'points': [
+            'Designed and implemented a full-featured expense tracker application.',
+            'Built a professional portfolio website to showcase projects and skills.',
+            'Gained proficiency in modern web technologies including React, Next.js, and Express.js.📈',
+            'Identified and resolved technical issues, optimizing application performance and usability.⚙️'
+        ],
+        'url': 'https://www.unifiedmentor.com/',
+    },
+    {
+        'company': 'Oasis Infobyte',
+        'role': 'Web developer Intern',
+        'duration': 'May 2023 - June 2023',
+        'logo': 'https://repository-images.githubusercontent.com/534965139/0f8a1508-fbd5-49af-89d7-27e3809353c0',
+        'points': [
+            'Successfully executed various JavaScript tasks, demonstrating proficiency in coding and problem-solving.',
+            'Gained experience with Git version control contributing to the development and maintenance of code repositories.📱.',
+            'Communicated technical concepts clearly and effectively, contributing to a productive team environment.',
+            'Gained in-depth knowledge of fundamental JavaScript concepts, including variables, functions, objects, and arrays.'
+        ],
+        'url': 'https://oasisinfobyte.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
